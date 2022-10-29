17a50c18d7fa8a23460a027d855bd1046ea1ba0c
dorivaldongoma <dorivaldongoma@gmail.com>
2022-10-29 05:57:08 +0100

    commit final

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.botao-menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
